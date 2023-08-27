c42a6523c99b49f3c060023df00bfad8b80daf0c
Rafiki <rafiki9703@gmail.com>
2023-08-27 21:38:34 +0200

    Adding project files to Git

---
+++ src/main/java/pl/rafiki/typer/TyperApplication.java
@@ -0,0 +1,13 @@
+package pl.rafiki.typer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rafiki/typer/TyperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rafiki.typer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
