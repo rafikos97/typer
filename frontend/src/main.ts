7be28092f22254546f2ad53bfded1cb52addedd3
Kuba Szczupaczynski <kuba.szczupaczynski@jamf.com>
2023-09-17 01:26:20 +0200

    Add AppComponent changes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppComponent)
+  .catch((err) => console.error(err));
