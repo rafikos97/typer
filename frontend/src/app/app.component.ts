ab3a138eb658e5b277d5bd75f890a979b4e59cdc
Kuba Szczupaczynski <kuba.szczupaczynski@jamf.com>
2023-09-17 01:20:20 +0200

    Revert ESLint

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [AppRoutingModule]
 })
 export class AppComponent {
   title = 'Typer';
