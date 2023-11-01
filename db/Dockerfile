262b401d29fbea8a90672c26093760b61343f216
Rafiki <rafiki9703@gmail.com>
2023-11-01 21:58:43 +0100

    Reorganized project files to enable deploying.

---
+++ db/Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Rafiki"
+
+ENTRYPOINT ["top", "-b"]
---
+++ db/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE typer_prototype;
+GRANT ALL PRIVILEGES ON DATABASE typer_prototype TO postgres;
