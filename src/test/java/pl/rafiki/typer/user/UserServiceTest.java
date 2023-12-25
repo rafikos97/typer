c5820edaa3e0c7216fc4d78b8b8525d8192a1007
Rafiki <virtusx90@gmail.com>
2023-12-25 13:36:00 +0100

    Updated UserRepositoryTest and UserServiceTest.

--- src/test/java/pl/rafiki/typer/user/UserServiceTest.java
+++ src/test/java/pl/rafiki/typer/user/UserServiceTest.java
@@ -9,10 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.rafiki.typer.security.models.Role;
 import pl.rafiki.typer.security.repositories.RoleRepository;
-import pl.rafiki.typer.user.exceptions.EmailAddressAlreadyTakenException;
-import pl.rafiki.typer.user.exceptions.IncorrectPasswordException;
-import pl.rafiki.typer.user.exceptions.UserDoesNotExistException;
-import pl.rafiki.typer.user.exceptions.UsernameAlreadyTakenException;
+import pl.rafiki.typer.user.exceptions.*;
 
 import java.util.Optional;
 
@@ -24,7 +21,6 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
     private UserService underTest;
     @Mock
     private UserRepository userRepository;
@@ -85,7 +81,7 @@ class UserServiceTest {
                 "Tester",
                 "username",
                 "a.tester@mail.com",
-                "password"
+                "Password123#"
         );
 
         given(roleRepository.findByAuthority(any())).willReturn(Optional.of(new Role()));
@@ -98,7 +94,9 @@ class UserServiceTest {
         ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
         verify(userRepository).save(userArgumentCaptor.capture());
         User capturedUser = userArgumentCaptor.getValue();
-        assertThat(capturedUser).isEqualTo(user);
+
+        RegisterUserDTO capturedUserAsRegisterUserDTO = new RegisterUserDTO(capturedUser.getFirstName(), capturedUser.getLastName(), capturedUser.getUsername(), capturedUser.getEmail(), capturedUser.getPassword());
+        assertThat(capturedUserAsRegisterUserDTO).isEqualTo(user);
     }
 
     @Test
@@ -109,7 +107,7 @@ class UserServiceTest {
                 "Tester",
                 "username",
                 "a.tester@mail.com",
-                "password"
+                "Password123#"
         );
 
         given(userRepository.existsByEmail(user.getEmail())).willReturn(true);
@@ -131,7 +129,7 @@ class UserServiceTest {
                 "Tester",
                 "username",
                 "a.tester@mail.com",
-                "password"
+                "Password123#"
         );
 
         given(userRepository.existsByUsername(user.getUsername())).willReturn(true);
@@ -141,6 +139,48 @@ class UserServiceTest {
         assertThatThrownBy(() -> underTest.registerUser(user))
                 .isInstanceOf(UsernameAlreadyTakenException.class)
                 .hasMessageContaining("Login already taken!");
+
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void willThrowWhenEmailDoesNotMatchRegex() {
+        // given
+        String email = "a.tester$mail.com";
+        RegisterUserDTO user = new RegisterUserDTO(
+                "Andrew",
+                "Tester",
+                "username",
+                email,
+                "Password123#"
+        );
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.registerUser(user))
+                .isInstanceOf(InvalidEmailException.class)
+                .hasMessageContaining("Provided email is invalid!");
+
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void willThrowWhenPasswordDoesNotMeetThePasswordPolicy() {
+        // given
+        String password = "password";
+        RegisterUserDTO user = new RegisterUserDTO(
+                "Andrew",
+                "Tester",
+                "username",
+                "a.tester@mail.com",
+                password
+        );
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.registerUser(user))
+                .isInstanceOf(PasswordDoesNotMatchPatternException.class)
+                .hasMessageContaining("Provided password does not meet password policy!");
 
         verify(userRepository, never()).save(any());
     }
@@ -165,7 +205,8 @@ class UserServiceTest {
         ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
         verify(userRepository).save(userArgumentCaptor.capture());
         User capturedUser = userArgumentCaptor.getValue();
-        assertThat(capturedUser).isEqualTo(updatedUser);
+        UserDTO capturedUserDTO = new UserDTO(capturedUser.getFirstName(), capturedUser.getLastName(), capturedUser.getUsername(), capturedUser.getEmail());
+        assertThat(capturedUserDTO).isEqualTo(updatedUser);
     }
 
     @Test
@@ -234,6 +275,29 @@ class UserServiceTest {
         assertThatThrownBy(() -> underTest.putUpdateUser(userId, updatedUser))
                 .isInstanceOf(UsernameAlreadyTakenException.class)
                 .hasMessageContaining("Username already taken!");
+
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void willThrowWhenEmailDoesNotMatchRegexDuringUpdate() {
+        // given
+        Long userId = 1L;
+        String email = "a.tester$mail.com";
+        UserDTO updatedUser = new UserDTO(
+                "Andrew",
+                "Tester",
+                "username",
+                email
+        );
+
+        given(userRepository.findById(userId)).willReturn(Optional.of(new User()));
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.putUpdateUser(userId, updatedUser))
+                .isInstanceOf(InvalidEmailException.class)
+                .hasMessageContaining("Provided email is invalid!");
 
         verify(userRepository, never()).save(any());
     }
@@ -340,8 +404,8 @@ class UserServiceTest {
         Long userId = 1L;
 
         PasswordDTO passwordDTO = new PasswordDTO(
-                "oldPassword",
-                "newPassword"
+                "oldPassword123#",
+                "newPassword123#"
         );
 
         given(userRepository.findById(userId)).willReturn(Optional.of(user));
@@ -360,8 +424,8 @@ class UserServiceTest {
         Long userId = 1L;
 
         PasswordDTO passwordDTO = new PasswordDTO(
-                "oldPassword",
-                "newPassword"
+                "oldPassword123#",
+                "newPassword123#"
         );
 
         given(userRepository.findById(userId)).willReturn(Optional.empty());
@@ -381,8 +445,8 @@ class UserServiceTest {
         Long userId = 1L;
 
         PasswordDTO passwordDTO = new PasswordDTO(
-                "oldPassword",
-                "newPassword"
+                "oldPassword123#",
+                "newPassword123#"
         );
 
         given(userRepository.findById(userId)).willReturn(Optional.of(user));
@@ -395,5 +459,57 @@ class UserServiceTest {
                 .hasMessageContaining("Old password is incorrect!");
 
         verify(user, never()).setPassword(any());
+    }
+
+    @Test
+    void willThrowWhenNewPasswordDoesNotMeetThePasswordPolicyWhileChangingPassword() {
+        // given
+        Long userId = 1L;
+
+        PasswordDTO passwordDTO = new PasswordDTO(
+                "oldPassword123#",
+                "password"
+        );
+
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(any(), any())).willReturn(true);
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.updatePassword(userId, passwordDTO))
+                .isInstanceOf(PasswordDoesNotMatchPatternException.class)
+                .hasMessageContaining("Provided password does not meet password policy!");
+
+        verify(user, never()).setPassword(any());
+    }
+
+    @Test
+    void canDeleteUser() {
+        // given
+        Long userId = 1L;
+
+        given(userRepository.existsById(userId)).willReturn(true);
+
+        // when
+        underTest.deleteUser(userId);
+
+        // then
+        verify(userRepository, times(1)).deleteById(any());
+    }
+
+    @Test
+    void willThrowWhenUserDoesNotExistWhileDeletingUser() {
+        // given
+        Long userId = 1L;
+
+        given(userRepository.existsById(userId)).willReturn(false);
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.deleteUser(userId))
+                .isInstanceOf(UserDoesNotExistException.class)
+                .hasMessageContaining("User with id: " + userId + " does not exist!");
+
+        verify(userRepository, never()).deleteById(any());
     }
 }
