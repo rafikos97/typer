cea0cca5dd1bcfde14a33a9d6b88196ade87c618
Rafiki <virtusx90@gmail.com>
2024-03-06 22:52:48 +0100

    Add ResponseEntity to ScoreboardController

--- src/main/java/pl/rafiki/typer/scoreboard/ScoreboardController.java
+++ src/main/java/pl/rafiki/typer/scoreboard/ScoreboardController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +32,8 @@ public class ScoreboardController {
     @Parameter(name = "Authorization", description = "Bearer token", required = true, in = ParameterIn.HEADER)
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping(path = "/{tournamentId}")
-    public List<ScoreboardDTO> getScoreboard(@PathVariable(name = "tournamentId") Long tournamentId) {
-        return scoreboardService.getScoreboard(tournamentId);
+    public ResponseEntity<List<ScoreboardDTO>> getScoreboard(@PathVariable(name = "tournamentId") Long tournamentId) {
+        List<ScoreboardDTO> scoreboardList = scoreboardService.getScoreboard(tournamentId);
+        return new ResponseEntity<>(scoreboardList, HttpStatus.OK);
     }
 }
